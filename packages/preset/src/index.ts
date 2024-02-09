121a9c80534019dea0950ebe8f035c79fdb66f7f
Em Dinh <ace53thntu@gmail.com>
2024-02-09 12:55:36 +0700

    define token fonts

--- packages/preset/src/index.ts
+++ packages/preset/src/index.ts
@@ -3,9 +3,11 @@ import { defineConfig } from "@pandacss/dev";
 
 // =================================================================
 import { tokens } from "./tokens";
+import { globalCss } from "./global-css";
 
 export const edmPandaPreset = defineConfig({
   presets: [pandaPreset],
+  globalCss,
   theme: {
     extend: {
       tokens,
--- packages/preset/src/tokens/index.ts
+++ packages/preset/src/tokens/index.ts
@@ -1,6 +1,10 @@
 import { defineTokens } from "@pandacss/dev";
+
+// =================================================================
 import { colors } from "./colors";
+import { fonts } from "./fonts";
 
 export const tokens = defineTokens({
   colors,
+  fonts,
 });
