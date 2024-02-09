54be44ab956b33d36cf2d3bf98a6ed2d0a3f6fc7
Em Dinh <ace53thntu@gmail.com>
2024-02-09 10:30:38 +0700

    migrate storybook to ts

--- apps/ui-demo/.storybook/main.ts
+++ apps/ui-demo/.storybook/main.ts
@@ -1,4 +1,5 @@
 import { join, dirname } from "path";
+import type { StorybookConfig } from "@storybook/react-vite";
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -8,12 +9,8 @@ function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, "package.json")));
 }
 
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
@@ -26,6 +23,9 @@ const config = {
   },
   docs: {
     autodocs: "tag",
+  },
+  core: {
+    builder: "@storybook/builder-vite", // 👈 The builder enabled here.
   },
 };
 export default config;
--- apps/ui-demo/panda.config.ts
+++ apps/ui-demo/panda.config.ts
@@ -6,12 +6,7 @@ export default defineConfig({
   preflight: true,
 
   // Where to look for your css declarations
-  include: [
-    "./node_modules/@edm/design-system/src/**/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./stories/**/*.{js,jsx,ts,tsx}",
-  ],
+  include: ["./node_modules/@edm/design-system/src/**/*.{js,jsx,ts,tsx}", "./stories/**/*.{js,jsx,ts,tsx}"],
 
   // Files to exclude
   exclude: [],
