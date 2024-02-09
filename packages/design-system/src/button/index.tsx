ba5e279970ed040302fd76730edbbc9f17ecc2d0
Em Dinh <ace53thntu@gmail.com>
2024-02-09 10:47:37 +0700

    define token colors

--- packages/design-system/src/button/index.tsx
+++ packages/design-system/src/button/index.tsx
@@ -1,5 +1,5 @@
 import { css } from "styled-system/css";
 
 export const Button = () => {
-  return <button className={css({ backgroundColor: "primary.900" })}>Hello world</button>;
+  return <button className={css({ backgroundColor: "semantics.warning.400" })}>Hello world</button>;
 };
